bba05c730175c099349d97bbf42385448437aaf3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-08 23:26:52 +0100

    backend completed

--- ticket_booking_app/go_server/cmd/api/main.go
+++ ticket_booking_app/go_server/cmd/api/main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
-	"ticket_booking_app/handlers"
-	"ticket_booking_app/repository"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"ticket_booking_app/config"
+	"ticket_booking_app/db"
+	"ticket_booking_app/handlers"
+	"ticket_booking_app/middlewares"
+	"ticket_booking_app/repositories"
+	"ticket_booking_app/services"
 )
 
 func main() {
+	envConfig := config.NewEnvConfig()
+	db := db.Init(envConfig, db.DBMigrator)
+
 	app := fiber.New(fiber.Config{
-		AppName: "Ticket Booking App",
+		AppName:      "Ticket-Booking",
 		ServerHeader: "Fiber",
 	})
 
-	// repository
-	eventRepository := repository.NewEventRepository(nil)
+	// Repositories
+	eventRepository := repositories.NewEventRepository(db)
+	ticketRepository := repositories.NewTicketRepository(db)
+	authRepository := repositories.NewAuthRepository(db)
 
-	// routing
+	// Service
+	authService := services.NewAuthService(authRepository)
+
+	// Routing
 	server := app.Group("/api")
+	handlers.NewAuthHandler(server.Group("/auth"), authService)
 
-	// handlers
-	handlers.NewEventHandler(server.Group("/event"),eventRepository)
+	privateRoutes := server.Use(middlewares.AuthProtected(db))
 
-	app.Listen(":3000")
+	handlers.NewEventHandler(privateRoutes.Group("/event"), eventRepository)
+	handlers.NewTicketHandler(privateRoutes.Group("/ticket"), ticketRepository)
+
+	app.Listen(fmt.Sprintf(":" + envConfig.ServerPort))
 }
---
+++ ticket_booking_app/go_server/db/auto-migrate.go
@@ -0,0 +1,10 @@
+package db
+
+import (
+	"ticket_booking_app/models"
+	"gorm.io/gorm"
+)
+
+func DBMigrator(db *gorm.DB) error {
+	return db.AutoMigrate(&models.Event{}, &models.Ticket{}, &models.User{})
+}
