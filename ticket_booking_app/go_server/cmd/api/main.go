75cb751c860bcaec76502f056b85de46ea46fd43
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-08 21:29:24 +0100

    built and configured the api endpoints

--- ticket_booking_app/go_server/cmd/api/main.go
+++ ticket_booking_app/go_server/cmd/api/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main(){}
+import (
+	"ticket_booking_app/handlers"
+	"ticket_booking_app/repository"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName: "Ticket Booking App",
+		ServerHeader: "Fiber",
+	})
+
+	// repository
+	eventRepository := repository.NewEventRepository(nil)
+
+	// routing
+	server := app.Group("/api")
+
+	// handlers
+	handlers.NewEventHandler(server.Group("/event"),eventRepository)
+
+	app.Listen(":3000")
+}
