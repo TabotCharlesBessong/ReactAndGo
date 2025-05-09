9bd7613b28d7a18bd2993b83ba5338e14a47631b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-05-09 16:38:33 +0100

    trying to fix the env not found issue

--- ticket_booking_app/go_server/config/config.go
+++ ticket_booking_app/go_server/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"path/filepath"
+
 	"github.com/caarlos0/env"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/joho/godotenv"
@@ -16,16 +18,20 @@ type EnvConfig struct {
 }
 
 func NewEnvConfig() *EnvConfig {
+	// Try to load .env from the current directory first
 	err := godotenv.Load()
-
 	if err != nil {
-		log.Fatalf("Unable to load .env: %e", err)
+		// If that fails, try to load from the project root
+		err = godotenv.Load(filepath.Join("..", ".env"))
+		if err != nil {
+			log.Fatalf("Unable to load .env file: %v", err)
+		}
 	}
 
 	config := &EnvConfig{}
 
 	if err := env.Parse(config); err != nil {
-		log.Fatalf("Unable to load variables from .env: %e", err)
+		log.Fatalf("Unable to load variables from .env: %v", err)
 	}
 
 	return config
