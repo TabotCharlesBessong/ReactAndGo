16cd905348976c449099ac823883b4b6d6672199
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-10-04 15:23:31 +0100

    setting up a gingonic server

--- movie_streaming/go_server/main.go
+++ movie_streaming/go_server/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-		fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to run server:", err)
+	}
 }
 
 // This is a simple Go server application that prints "Hello, World!" to the console.
